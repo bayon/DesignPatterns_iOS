f930499b3a5c78c540e0e006125356b01666f2c0
Bayon <bforte@indatus.com>
2014-03-10 13:37:10 -0400

    Initial Commit

---
+++ MVC/MVC/FBFViewController.h
@@ -0,0 +1,13 @@
+//
+//  FBFViewController.h
+//  MVC
+//
+//  Created by Bayon Forte on 3/10/14.
+//  Copyright (c) 2014 Mocura. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FBFViewController : UIViewController
+
+@end
